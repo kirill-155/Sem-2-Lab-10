eb11bae54cc2c00f26c44848f260b6f4e10413b2
kirill-155 <57168550+kirill-155@users.noreply.github.com>
2024-05-05 23:58:18 +0300

    add GlutSettings

--- Лабораторная работа №10/Лабораторная работа №10.cpp
+++ Лабораторная работа №10/Лабораторная работа №10.cpp
@@ -2,8 +2,24 @@
 #include "Point.h"
 #include "Line.h"
 #include "Polygon.h"
+#include "GlutSettings.h"
 
-int main()
+void solve() {
+	display();
+
+}
+
+int main(int argc, char** argv)
 {
+	setlocale(LC_ALL, "ru");
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(width, height);// Размер окна
+	glutInitWindowPosition(0, 0);// позиция открытия
+	glutCreateWindow("Лабораторная работа 10");// Заголовок окна
+	init();
+	glutDisplayFunc(solve);
+	glutMainLoop();
 
 }
