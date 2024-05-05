7210b92d2b722d04937b09329f3f3d1da0d4c7cb
kirill-155 <57168550+kirill-155@users.noreply.github.com>
2024-05-05 16:52:36 +0300

    add Shape

--- Лабораторная работа №10/Line.h
+++ Лабораторная работа №10/Line.h
@@ -2,16 +2,34 @@
 #include <iostream>
 #include "Point.h"
 
-class line {
-	double a, b, c;
+class Line {
+	double A, B, C;
 
 public:
-	line(point p1, point p2) {
-		p1.line(p2, a, b, c);
+	Line(point p1, point p2) {
+		p1.line(p2, A, B, C);
 	}
 
 	template<typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
-	line(T k, T x) {
-
+	Line(T k, T b) {
+		A = k;
+		C = b;
+		B = -1;
 	}
+
+	template<typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+	Line(point p, T k) {
+		A = k;
+		C = p.y - k * p.x;
+		B = -1;
+	}
+
+	bool operator==(const Line& l) const {
+		return A == l.A && B == l.B && C == l.C;
+	}
+
+	bool operator!=(const Line& l) const {
+		return !(*this == l);
+	}
+
 };
---
+++ Лабораторная работа №10/Shape.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include "Point.h"
+#include "Line.h"
+
+// Абстрактный класс Shape — фигура.
+class Shape {
+public:
+	virtual double perimeter() const = 0;// периметр
+	virtual double area() const = 0;// площадь
+	virtual bool operator==(const Shape& another) const = 0;// совпадает ли эта фигура с другой
+	bool operator!=(const Shape& another) const {
+		return !(*this == another);
+	}
+	virtual bool isCongruentTo(const Shape& another) const = 0;// равна ли эта фигура другой в геометрическом смысле
+	virtual bool isSimilarTo(const Shape& another) const = 0;  // подобна ли эта фигура другой
+	virtual bool containsPoint(point point) const = 0;// находится ли точка внутри фигуры
+	virtual bool rotate(point center, double angle) const = 0;// поворот на угол (в градусах, против часовой стрелки) относительно точки
+	virtual bool reflex(point center) const = 0;// симметрию относительно точки
+	virtual bool reflex(Line axis) const = 0;// симметрию относительно прямой
+	virtual bool scale(point center, double coefficient) const = 0;// гомотетию с коэффициентом coefficient и центром center
+};
