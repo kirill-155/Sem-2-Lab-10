9f9dd5cc1fc47e61c96c918caf722fad960ce994
kirill-155 <57168550+kirill-155@users.noreply.github.com>
2024-05-07 01:37:13 +0300

    add Triangle

--- Лабораторная работа №10/Square.h
+++ Лабораторная работа №10/Square.h
@@ -7,12 +7,12 @@ public:
 	Square(point a, point b) : Rectangle(a, b, 1) {}
 
 	// Методы
-    Circle circumscribedCircle() const {
+    Circle circumscribedCircle() const {// Круг описанный
         vector<point> ver = getVertices();
         return { center(), (ver[0], ver[1]).dist() / 2 };
     }
 
-    Circle inscribedCircle() const {
+    Circle inscribedCircle() const {// Круг вписанный
         vector<point> ver = getVertices();
         return { center(), (ver[0], ver[2]).dist() / 2 };
     }
---
+++ Лабораторная работа №10/Triangle.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Polygon.h"
+#include "Circle.h"
+
+class Triangle : public Polygon {
+    using Polygon::Polygon;
+
+public:
+    Circle circumscribedCircle() const {// Круг описанный
+        Line first = orthogonal(Line(Vertices[0], Vertices[1]), point((Vertices[0].x + Vertices[1].x) / 2, (Vertices[0].y + Vertices[1].y) / 2));
+        Line second = orthogonal(Line(Vertices[1], Vertices[2]), point((Vertices[1].x + Vertices[2].x) / 2, (Vertices[1].y + Vertices[2].y) / 2));
+        point center = intersect(first, second);
+        return { center, (center, Vertices[0]).dist()};
+    }
+
+    Circle inscribedCircle() const {// Круг вписанный
+        double a = (Vertices[0], Vertices[1]).dist();
+        double b = (Vertices[1], Vertices[2]).dist();
+        double c = (Vertices[2], Vertices[0]).dist();
+        point tmp;
+        tmp.x = (Vertices[2].x * a + Vertices[0].x * b + Vertices[1].x * c) / (a + b + c);
+        tmp.y = (Vertices[2].y * a + Vertices[0].y * b + Vertices[1].y * c) / (a + b + c);
+        return { tmp, 2 * area() / (a + b + c) };
+    }
+
+    point centroid() const {
+        double x = (Vertices[0].x + Vertices[1].x + Vertices[2].x) / 3;
+        double y = (Vertices[0].y + Vertices[1].y + Vertices[2].y) / 3;
+        return { x, y };
+    }
+
+    point orthocenter() const {
+        Line first = orthogonal(Line(Vertices[0], Vertices[1]), Vertices[2]);
+        Line second = orthogonal(Line(Vertices[1], Vertices[2]), Vertices[0]);
+        return intersect(first, second);
+    }
+
+    Line EulerLine() const {
+        return { circumscribedCircle().center(), orthocenter() };
+    }
+
+    Circle ninePointsCircle() const {
+        point first = circumscribedCircle().center();
+        point second = orthocenter();
+        point center = point((first.x + second.x) / 2, (first.y + second.y) / 2);
+        return { center, circumscribedCircle().radius() / 2 };
+    }
+};
